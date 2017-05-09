c816a4ce02868f73c80e76473a88e65232e955e0
taijiji <tuty0630@gmail.com>
2017-05-09 16:37:50 +0900

    Initial commit. Falked by https://github.com/taijiji/scenarioJUNOS

---
+++ router.py
@@ -0,0 +1,267 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Jinja2 Template Engine
+from jinja2 import Template, Environment
+
+## PyEZ
+#from jnpr.junos import Device
+#from jnpr.junos.utils.config import Config
+
+## JSNAPy
+#from jnpr.jsnapy import SnapAdmin
+
+# arranged print
+from pprint import pprint, pformat
+
+
+class Router:
+    def __init__(self, hostname, model, ipaddress, username, password):
+        self.hostname = hostname
+        self.model = model
+        self.username = username
+        self.password = password
+        self.ipaddress = ipaddress
+        #self.device = Device(host=ipaddress, user=username, password=password)
+        #self.snap = SnapAdmin()
+
+    def open(self):
+        #self.device.open()
+        #self.device.bind(cu=Config)
+        pass
+
+    def lock(self):
+        #self.device.cu.lock()
+        pass
+
+    def unlock(self):
+        #self.device.cu.unlock()
+        pass
+
+    def close(self):
+        #self.device.close()
+        pass
+
+    def commit(self):
+        #return self.device.cu.commit()
+        pass
+
+    def rollback(self):
+        #return self.device.cu.rollback()
+        pass
+
+    def diff_config(self):
+        #if self.device.cu.diff():
+        #    message = self.device.cu.diff()
+        #else:
+        #    message = ''
+        #return message
+        pass
+
+    def commit_check(self):
+        #return self.device.cu.commit_check()
+        pass
+
+    def load_config(self, operation_name, operation_param=None):
+        '''
+        set_result = False
+        message = ''
+        if operation_name == 'set_add_interface':
+            template_filename = './set_templates/add_interface.jinja2'
+            tamplate_param = operation_param
+        elif operation_name == 'set_add_bgp_neighbor':
+            template_filename = './set_templates/add_bgp_neighbor.jinja2'
+            tamplate_param = operation_param
+        elif operation_name == 'set_add_bgp_policy_external':
+            template_filename =\
+                './set_templates/add_bgp_policy_external.jinja2'
+            tamplate_param = operation_param
+        else:
+            pass
+
+        config_txt = self.generate_from_jinja2(
+            template_filename,
+            tamplate_param)
+
+        self.device.cu.load(
+            template_path=template_filename,
+            template_vars=tamplate_param,
+            format="text",
+            merge=True)
+
+        message = config_txt
+        set_result = True
+
+        return set_result, message
+        '''
+        pass
+
+    def nwtest(self, operation_name, operation_param=None):
+        '''
+        nwtest_result = False
+        message = ''
+        template_filename = ''
+
+        if operation_name == 'nwtest_hostname':
+            template_filename =\
+                './nwtest_templates/%s.jinja2' % (operation_name)
+            tamplate_param = {'hostname': self.hostname}
+            nwtest_filename =\
+                './nwtests/' +\
+                operation_name + '_' +\
+                self.hostname +\
+                '.yml'
+
+        elif operation_name == 'nwtest_model':
+            template_filename =\
+                './nwtest_templates/%s.jinja2' % (operation_name)
+            tamplate_param = {'model': self.model}
+            nwtest_filename =\
+                './nwtests/' +\
+                operation_name + '_' +\
+                self.hostname +\
+                '.yml'
+
+        elif operation_name == 'nwtest_interface':
+            template_filename =\
+                './nwtest_templates/%s.jinja2' % (operation_name)
+            tamplate_param = operation_param
+            nwtest_filename =\
+                './nwtests/' +\
+                operation_name + '_' +\
+                operation_param['interface_name'].replace('/', '-') + '_' +\
+                operation_param['interface_status'] +\
+                '.yml'
+
+        elif operation_name == 'nwtest_bgp_neighbor':
+            template_filename =\
+                './nwtest_templates/%s.jinja2' % (operation_name)
+            tamplate_param = operation_param
+            nwtest_filename =\
+                './nwtests/' +\
+                operation_name + '_' +\
+                operation_param['neighbor_address_ipv4'].replace('.', '-') + '_' +\
+                operation_param['neighbor_status'] +\
+                '.yml'
+
+        elif operation_name == 'nwtest_bgp_received_route':
+            template_filename =\
+                './nwtest_templates/%s.jinja2' % (operation_name)
+            tamplate_param = operation_param
+            nwtest_filename =\
+                './nwtests/' +\
+                operation_name + '_' +\
+                operation_param['neighbor_address_ipv4'].replace('.', '-') + '_' +\
+                operation_param['received_route_address_ipv4'].replace('.', '-') +\
+                '.yml'
+
+        elif operation_name == 'nwtest_bgp_advertised_route':
+            template_filename =\
+                './nwtest_templates/%s.jinja2' % (operation_name)
+            tamplate_param = operation_param
+            nwtest_filename =\
+                './nwtests/' +\
+                operation_name + '_' +\
+                operation_param['neighbor_address_ipv4'].replace('.', '-') + '_' +\
+                operation_param['advertised_route_address_ipv4'].replace('.', '-') +\
+                '.yml'
+
+        elif operation_name == 'nwtest_ping':
+            template_filename =\
+                './nwtest_templates/%s.jinja2' % (operation_name)
+            tamplate_param = operation_param
+            nwtest_filename =\
+                './nwtests/' +\
+                operation_name + '_' +\
+                operation_param['target_ipaddress'].replace('.', '-') +\
+                '.yml'
+
+        else:
+            pass
+
+        self.generate_nwtestfile(
+            template_filename=template_filename,
+            template_param=tamplate_param,
+            nwtest_filename=nwtest_filename)
+
+        jsnapy_conf = 'nwtests:' + '\n' +\
+                      '  - %s' % (nwtest_filename)
+
+        snapcheck_dict = self.snap.snapcheck(data=jsnapy_conf, dev=self.device)
+
+        for snapcheck in snapcheck_dict:
+            if snapcheck.result == 'Passed':
+                nwtest_result = True
+
+                if operation_name == 'nwtest_bgp_received_route':
+                    expected_value = '%s/%s' % (
+                        operation_param['received_route_address_ipv4'],
+                        operation_param['received_route_subnet_ipv4'])
+                    acutual_value =\
+                        snapcheck.test_details.values()[0][0]['passed'][0]['pre'].keys()[0]
+
+                elif operation_name == 'nwtest_bgp_advertised_route':
+                    expected_value = '%s/%s' % (
+                        operation_param['advertised_route_address_ipv4'],
+                        operation_param['advertised_route_subnet_ipv4'])
+                    acutual_value =\
+                        snapcheck.test_details.values()[0][0]['passed'][0]['pre'].keys()[0]
+
+                else:
+                    expected_value =\
+                        snapcheck.test_details.values()[0][0]['expected_node_value']
+                    acutual_value =\
+                        snapcheck.test_details.values()[0][0]['passed'][0]['actual_node_value']
+
+                message =\
+                    'nwtest file      : %s\n' % nwtest_filename +\
+                    'expected value : %s\n' % (expected_value) +\
+                    'acutual  value : %s' % (acutual_value)
+
+            elif snapcheck.result == 'Failed':
+                nwtest_result = False
+
+                if operation_name == 'nwtest_bgp_received_route':
+                    expected_value = '%s/%s' % (
+                        operation_param['received_route_address_ipv4'],
+                        operation_param['received_route_subnet_ipv4'])
+                    acutual_value = 'None'
+
+                elif operation_name == 'nwtest_bgp_advertised_route':
+                    expected_value = '%s/%s' % (
+                        operation_param['advertised_route_address_ipv4'],
+                        operation_param['advertised_route_subnet_ipv4'])
+                    acutual_value = 'None'
+
+                else:
+                    expected_value =\
+                        snapcheck.test_details.values()[0][0]['expected_node_value']
+                    acutual_value =\
+                        snapcheck.test_details.values()[0][0]['failed'][0]['actual_node_value']
+
+                message = 'nwtest file      : %s\n' % nwtest_filename +\
+                    'expected value : %s\n' % (expected_value) +\
+                    'acutual  value : %s' % (acutual_value)
+
+        return nwtest_result, message
+        '''
+        pass
+
+    def generate_nwtestfile(self, template_filename, template_param, nwtest_filename):
+        '''
+        nwtest_yml = self.generate_from_jinja2(template_filename, template_param)
+        # write nwtest file (YAML format)
+        with open(nwtest_filename, 'w') as f:
+            f.write(nwtest_yml)
+        '''
+        pass
+    def generate_from_jinja2(self, template_filename, template_param):
+        '''
+        # read template file (jinja2 format)
+        with open(template_filename, 'r') as f:
+            template_jinja2 = f.read()
+
+        # generate nwtest file from template file
+        return Environment().from_string(template_jinja2).render(template_param)
+        '''
+        pass
---
+++ run_scenario.py
@@ -0,0 +1,153 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import yaml
+from pprint import pprint, pformat
+from argparse import ArgumentParser
+import time
+
+# clolor font
+import colorama
+from colorama import Fore, Back, Style
+
+from router import Router
+
+def main():
+    """main function."""
+
+    # Parse argment
+    parser = ArgumentParser(description='run scenario_file')
+    parser.add_argument('-f', '--file',
+                        type=str,
+                        help='scenario file',
+                        required=True)
+    args = parser.parse_args()
+
+    # Set color font
+    colorama.init(autoreset=True)
+
+    # Read router infomation file
+    try:
+        with open(args.file, 'r') as f:
+            param_yaml = f.read()
+    except (IOError, IndexError):
+        sys.stderr.write('Cannot open file : ' + args.file + '\n')
+        sys.exit(1)
+
+    # Convert yaml format to python_type
+    try:
+        param = yaml.load(param_yaml)
+    except ValueError as error:
+        sys.stderr.write('YAML format error : \n')
+        sys.stderr.write(param_yaml)
+        sys.stderr.write(str(error))
+        sys.exit(1)
+
+    router1 = Router(
+        hostname    = param['hosts']['hostname'],
+        model       = param['hosts']['model'],
+        ipaddress   = param['hosts']['management_ipaddress'],
+        username    = param['hosts']['username'],
+        password    = param['hosts']['password'])
+
+    print('########## Run Senario : ' + args.file + ' ##########')
+
+    print('operator : %s'       % (param['operator']))
+    print('operation_date : %d' % (param['operation_date']))
+    print('hostname : %s'       % (param['hosts']['hostname']))
+    print('model : %s'          % (param['hosts']['model']))
+    print('purpose :')
+    print(param['purpus'])
+    
+    print('Connect to ' + param['hosts']['hostname'] + ' : ', end='')
+    router1.open()
+    print(Fore.GREEN + 'OK')
+
+    print('Lock configure mode : ', end='')
+    router1.lock()
+    print(Fore.GREEN + 'OK')
+
+    for scenario_param in param['scenario']:
+        if isinstance(scenario_param, dict):
+            operation_name  = scenario_param.keys()[0]
+            operation_param = scenario_param[operation_name]
+        else:
+            operation_name = scenario_param
+            operation_param = None
+
+        if 'nwtest_' in operation_name:
+            print('Test on < %s > : ' % (operation_name), end='')
+            result, message =\
+                router1.snaptest(operation_name, operation_param)
+
+            if result:
+                print(Fore.GREEN + 'OK')
+                print(Fore.GREEN + message)
+            else:
+                print(Fore.RED + 'NG')
+                print(Fore.RED + message)
+
+        elif 'set_' in operation_name:
+            print('Load config on < %s > : ' % (operation_name), end='')
+            result, message =\
+                router1.load_config(operation_name, operation_param)
+            if result:
+                print(Fore.GREEN + 'OK')
+                print('-'*30)
+                print('Set config: ')
+                print('-'*30)
+                print(Fore.YELLOW + message)
+                print('-'*30)
+            else:
+                print(Fore.RED + 'NG')
+                print(Fore.RED + message)
+
+            print('Diff config :')
+            message = router1.diff_config()
+            print('-'*30)
+            print(Fore.YELLOW + message)
+            print('-'*30)
+
+            print('Commit Check : ', end='')
+            if router1.commit_check():
+                print(Fore.GREEN + 'OK')
+            else:
+                print(Fore.RED + 'NG')
+
+            print(Fore.YELLOW + "Do you commit? y/n")
+            choice = raw_input().lower()
+            if choice == 'y':
+                print('Commit : ', end='')
+                if router1.commit():
+                    print(Fore.GREEN + 'OK')
+                else:
+                    print(Fore.RED + 'NG')
+            else:
+                print('Rollback : ', end='')
+                if router1.rollback():
+                    print(Fore.GREEN + 'OK')
+                else:
+                    print(Fore.RED + 'NG')
+
+        elif operation_name == 'sleep_10sec':
+            print('sleep 10 sec : ', end='')
+            time.sleep(10)
+            print(Fore.GREEN + 'OK')
+
+        else:
+            print('Cannnot run operation : ' + operation_name)
+
+    print('Unlock configure mode : ', end='')
+    router1.unlock()
+    print(Fore.GREEN + 'OK')
+
+    print('Close the connection to ' + param['hosts']['hostname'] + ' : ', end='')
+    router1.close()
+    print(Fore.GREEN + 'OK')
+
+    print('########## End Senario : ' + args.file + ' ##########')
+
+if __name__ == '__main__':
+    main()
+
